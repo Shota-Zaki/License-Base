746a06b8ab9378eb22ef3d2bd6e2e4951dcc08b2
Shota-Zaki <shota690@gmail.com>
2026-06-11 14:21:50 +0900

    feat: register MVP account controllers

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,13 +4,19 @@ import { LabsController } from './modules/labs/labs.controller.js';
 import { CoursesController } from './modules/courses/courses.controller.js';
 import { QuestionsController } from './modules/questions/questions.controller.js';
 import { PracticeSetsController } from './modules/practice-sets/practice-sets.controller.js';
+import { AttemptsController } from './modules/attempts/attempts.controller.js';
+import { MeController } from './modules/me/me.controller.js';
+import { PlansController } from './modules/plans/plans.controller.js';
 
 const controllers = [
   HealthController,
   LabsController,
   CoursesController,
   QuestionsController,
-  PracticeSetsController
+  PracticeSetsController,
+  AttemptsController,
+  MeController,
+  PlansController
 ];
 
 @Module({
