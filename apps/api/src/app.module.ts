0de183324a9d95d6e93a8727f16c35b961eef10d
Shota-Zaki <shota690@gmail.com>
2026-06-11 12:16:25 +0900

    feat: register practice controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,10 +3,19 @@ import { HealthController } from './modules/health/health.controller.js';
 import { LabsController } from './modules/labs/labs.controller.js';
 import { CoursesController } from './modules/courses/courses.controller.js';
 import { QuestionsController } from './modules/questions/questions.controller.js';
+import { PracticeSetsController } from './modules/practice-sets/practice-sets.controller.js';
+
+const controllers = [
+  HealthController,
+  LabsController,
+  CoursesController,
+  QuestionsController,
+  PracticeSetsController
+];
 
 @Module({
   imports: [],
-  controllers: [HealthController, LabsController, CoursesController, QuestionsController],
+  controllers,
   providers: []
 })
 export class AppModule {}
